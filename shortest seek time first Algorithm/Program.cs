132864f757fbd2b34966a5514e6ee36093d763bc
todim <todim@DESKTOP-CHTNRQ5>
2021-09-02 21:03:27 +0100

    Add project files.

---
+++ shortest seek time first Algorithm/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace shortest_seek_time_first_Algoritm
+{
+    class program
+    {
+
+        static int leftCheck(List<List<int>> llist , int currIndex)
+        {
+            int leftdiff = 0;
+            for(int i = currIndex - 1;i>=0;i--)
+            {
+                if(llist[1][i] != 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    leftdiff = llist[0][currIndex] - llist[0][i];
+                    i = -1;
+                }
+                
+            }
+
+            return leftdiff;
+
+
+            
+        }
+
+        static int rightCheck(List<List<int>> llist, int currIndex)
+        {
+            int rightdiff = 0;
+            for (int i = currIndex + 1; i < llist[0].Count ; i++)
+            {
+                if (llist[1][i] != 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    rightdiff = Math.Abs(llist[0][currIndex] - llist[0][i]) ;
+                    break;
+                }
+            }
+
+            return rightdiff;
+
+
+
+        }
+        static int sstf(List<List<int>> llist , int len , int sPos)
+        {
+            int currIndex = llist[0].IndexOf(sPos);
+            int sTime = 0;
+
+            while(llist[1].Contains(0))
+            {
+                //int leftdiff = llist[0][currIndex] - llist[0][currIndex - 1];
+                //int rightdiff = Math.Abs((llist[0][currIndex] - llist[0][currIndex + 1]));
+                //if (((leftdiff < rightdiff) && (llist[1][currIndex - 1] == 0 && llist[1][currIndex + 1] == 0)))
+                //{
+                //    sTime += leftdiff;
+                //    currIndex = llist[0].IndexOf();
+                //}
+                int left = leftCheck(llist, currIndex);
+                int right = rightCheck(llist, currIndex);
+                
+                if ((left!=0) && left < right )
+                {
+                    sTime += left;
+                    llist[1][currIndex] = 1;
+                    currIndex = llist[0].IndexOf(llist[0][currIndex] - left);
+                    //currIndex = 0;
+                    
+                }
+                else if((right != 0) && (right < left) )
+                {
+                    sTime += right;
+                    llist[1][currIndex] = 1;
+                    currIndex = llist[0].IndexOf(llist[0][currIndex] + right);
+                    
+                }
+                else if (right == 0 && right < left)
+                {
+                    sTime += left;
+                    llist[1][currIndex] = 1;
+                    currIndex = llist[0].IndexOf(llist[0][currIndex] - left);
+                }
+                else if (left == 0 && left < right)
+                {
+                    sTime += right;
+                    llist[1][currIndex] = 1;
+                    currIndex = llist[0].IndexOf(llist[0][currIndex] + right);
+                }
+                else if(right == left)
+                {
+                    llist[1][currIndex] = 1;
+                    currIndex = llist[0].IndexOf(llist[0][currIndex] - left);
+                }
+
+                Console.WriteLine(string.Join(",", llist[1]));
+
+
+
+
+                
+            }
+
+
+            return sTime;
+        }
+
+        static void Main(string[] args)
+        {
+            List<List<int>> sList = new List<List<int>>();
+            int length;
+
+            Console.Write("enter sequence : ");
+            string sequence = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("enter start position: ");
+            int sPosition = int.Parse(Console.ReadLine());
+            length = sequence.Split(',').Length;
+
+            sList.Add(sequence.Trim(' ').Split(',').ToList().Select(sTemp => int.Parse(sTemp)).ToList());
+            string sTemp = "0";
+            for(int i = 0; i < length - 1;i++)
+            {
+                sTemp += ",0";  
+            }
+            sList.Add(sTemp.Trim(' ').Split(',').ToList().Select(sTemp => int.Parse(sTemp)).ToList());
+
+            //Console.WriteLine(String.Join(",", sList[1]) + " "  + length);
+
+            sList[0].Sort();
+
+            int seekTime = sstf(sList, length, sPosition);
+            Console.WriteLine($"the seek time is : {seekTime}ms");
+
+
+
+
+        }
+    }
+}
